9a34fccc4a9d10085293ff952d37006f8cc7229a
wadwdwd1 <imrbk123abc@proton.me>
2025-04-20 15:17:57 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,4 @@ MTOD="${MTOD:-'wadmc.site'}"
 
 sed -i 's/${SERVER}/'"$SERVER"'/g' config.yml
 
-cd plugins
-cd EaglercraftXBungee
-
-sed -i 's/${MTOD}/'"$MTOD"'/g' listeners.yml
-
-cd /
-cd bungee
-
 java -Xmx1024M -Xms1024M -jar bungee.jar

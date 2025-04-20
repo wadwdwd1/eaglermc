d7438d12d542e96a6b52d42cb6816abae3bfca40
wadwdwd1 <imrbk123abc@proton.me>
2025-04-20 15:54:11 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,9 @@ MTOD="${MTOD:-'wadmc.site'}"
 sed -i 's/${SERVER}/'"$SERVER"'/g' config.yml
 
 java -Xmx1024M -Xms1024M -jar bungee.jar
+
+cd ..
+cd serv2
+
+java -Xmx1024M -Xms1024M -jar server.jar
+
